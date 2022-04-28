f9e76f40b2d0dbc7f2aa1684574b0d13608bc789
m1stadev <adamhamdi31@gmail.com>
2022-04-28 15:26:35 -0500

    [device] Update - Use property setters for verifying data - generator -> boot_nonce - Update some variable names

--- pytss/device.py
+++ pytss/device.py
@@ -12,30 +12,103 @@ class Device:
         identifier: str,
         ecid: str,
         board: str = None,
-        generator: str = None,
+        boot_nonce: str = None,
         apnonce: str = None,
         sepnonce: str = None,
     ):
-        await self._fetch_device_info(identifier)
-        self.ecid = self._verify_ecid(ecid)
+        self.ecid = ecid
 
         if getattr(self, 'board', None) is None and board is not None:
             self.board = self._verify_board(board)
 
-        self.generator = self._verify_generator(generator) if generator else None
+        self.boot_nonce = boot_nonce
 
-        self.apnonce = self._verify_nonce(apnonce) if apnonce else None
+        self.apnonce = apnonce
 
-        self.sepnonce = self._verify_nonce(sepnonce) if sepnonce else None
+        self.sepnonce = sepnonce
 
-        if (generator and apnonce) and (not 0x8020 <= self.cpid < 0x8900):
+        if (boot_nonce and apnonce) and (not 0x8020 <= self.cpid < 0x8900):
             await self._verify_apnonce_pair()
 
         return self
 
     @property
-    def is_64bit(self) -> bool:
-        return 0x8960 <= self.cpid < 0x8900
+    def apnonce(self) -> bytes:
+        return self._apnonce
+
+    @apnonce.setter
+    def apnonce(self, apnonce: Optional[Union[bytes, str]]) -> None:
+        apnonce_len = 32 if 0x8010 <= self.cpid < 0x8900 else 20
+
+        if apnonce is not None:
+            if isinstance(apnonce, str):  # Assume hexadecimal
+                try:
+                    apnonce = bytes.fromhex(apnonce)
+                except TypeError:
+                    raise DeviceError('Invalid ApNonce provided.')
+
+            if len(apnonce) != apnonce_len:
+                raise DeviceError('Invalid ApNonce provided.')
+        else:
+            pass  # generate apnonce here
+
+        self._apnonce = apnonce
+
+    @property
+    def ecid(self) -> int:
+        return self._ecid
+
+    @ecid.setter
+    def ecid(self, ecid: Union[int, str]) -> None:
+        if isinstance(ecid, str):  # Assume hexadecimal
+            try:
+                ecid = int(ecid, 16)
+            except ValueError:
+                raise DeviceError('Invalid ECID provided.')
+
+        self._ecid = ecid
+
+    @property
+    def boot_nonce(self) -> bytes:
+        return self._boot_nonce
+
+    @boot_nonce.setter
+    def boot_nonce(self, boot_nonce: Optional[Union[str, bytes]]) -> None:
+        if boot_nonce is not None:
+            if isinstance(boot_nonce, str):  # Assume hexadecimal
+                try:
+                    boot_nonce = bytes.fromhex(boot_nonce)
+                except TypeError:
+                    raise DeviceError('Invalid Boot Nonce provided.')
+
+        else:
+            pass  # make random bytes here
+
+        self._boot_nonce = boot_nonce
+
+    @property
+    def sepnonce(self) -> bytes:
+        return self._sepnonce
+
+    @sepnonce.setter
+    def sepnonce(self, sepnonce: Optional[Union[bytes, str]]) -> None:
+        if sepnonce is not None:
+            if isinstance(sepnonce, str):
+                try:
+                    sepnonce = bytes.fromhex(sepnonce)
+                except TypeError:
+                    raise DeviceError('Invalid SepNonce provided.')
+
+            if len(sepnonce) != 20:
+                raise DeviceError('Invalid SepNonce provided.')
+        else:
+            pass  # generate sepnonce here
+
+        self._sepnonce = sepnonce
+
+    @property
+    def supports_img4(self) -> bool:
+        return not 0x8900 < self.cpid < 0x8955
 
     async def _fetch_device_info(self, identifier: str) -> None:
         async with aiohttp.ClientSession() as session, session.get(
@@ -70,24 +143,6 @@ class Device:
             self.cpid = valid_boards[0]['cpid']
             self.bdid = valid_boards[0]['bdid']
 
-    # Data verification functions
-    def _verify_ecid(self, ecid: str) -> str:
-        try:
-            int(ecid, 16)  # Make sure the ECID provided is hexadecimal, not decimal
-        except (ValueError, TypeError):
-            raise DeviceError('Invalid ECID provided.')
-
-        ecid = hex(int(ecid, 16)).removeprefix('0x')
-        if len(ecid) not in (
-            10,
-            11,
-            13,
-            14,
-        ):  # All hex ECIDs without zero-padding are either 10, 11, 13, or 14 characters long
-            raise DeviceError('Invalid ECID provided.')
-
-        return ecid
-
     async def _verify_board(self, board: str) -> str:
         for b in [
             b
@@ -104,53 +159,15 @@ class Device:
 
         return board
 
-    def _verify_generator(self, generator: str) -> str:
-        if generator is None:
-            return
-
-        if not generator.startswith('0x'):  # Generator must start wth '0x'
-            raise DeviceError('Invalid generator provided.')
-
-        if (
-            len(generator) != 18
-        ):  # Generator must be 18 characters long, including '0x' prefix
-            raise DeviceError('Invalid generator provided.')
-
-        try:
-            int(generator, 16)  # Generator must be hexadecimal
-        except:
-            raise DeviceError('Invalid generator provided.')
-
-        return generator.lower()
-
-    def _verify_nonce(self, nonce: str) -> str:
-        if any(arg is None for arg in (self.cpid, nonce)):
-            return
-
-        try:
-            int(nonce, 16)
-        except ValueError or TypeError:
-            raise DeviceError('Invalid nonce provided.')
-
-        if 0x8010 <= self.cpid < 0x8900:  # A10+ device nonces are 64 characters long
-            nonce_len = 64
-        else:  # A9 and below device nonces are 40 characters
-            nonce_len = 40
-
-        if len(nonce) != nonce_len:
-            raise DeviceError('Invalid nonce provided.')
-
-        return nonce.lower()
-
     def __verify_apnonce_pair(self) -> bool:
-        gen = bytes.fromhex(self.generator.removeprefix('0x'))
+        boot_nonce = bytes.fromhex(self.boot_nonce.removeprefix('0x'))
         if len(self.apnonce) == 64:
-            gen_hash = sha384(gen).hexdigest()[:-32]
+            apnonce = sha384(boot_nonce).hexdigest()[:-32]
         elif len(self.apnonce) == 40:
-            gen_hash = sha1(gen).hexdigest()
+            apnonce = sha1(boot_nonce).hexdigest()
 
-        if gen_hash != self.apnonce:
-            raise DeviceError('Invalid generator-ApNonce pair provided.')
+        if apnonce != self.apnonce:
+            raise DeviceError('Invalid Boot Nonce-ApNonce pair provided.')
 
     async def _verify_apnonce_pair(self) -> Optional[bool]:
         await asyncio.to_thread(self.__verify_apnonce_pair)
